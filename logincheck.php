50f7ebd79c988906d7c5ac0e7c9e7752a4a4a4ae
OX SoftwareS <ramanathan@oxsoftwares.com>
2017-07-27 10:45:49 +0530

    login page with validation

--- logincheck.php
+++ logincheck.php
@@ -1,15 +1,20 @@
 <?php
-include('dbconnection.php');
-$u_name = $_POST[Firstname];
-$u_password = md5($_POST[Password]);
-$selectquery = "select * from Phptask where name='$u_name' and password='$u_password'";
-$result = mysqli_query($dbcon, $selectquery);
-if (mysqli_num_rows($result)== 1) {
-    session_start();
-    $_SESSION['username']=$u_name;
-    header('location:account.php');
 
-} else {
-    echo "<h2>'wrong user'</h2>";
-}
+include('dbconnection.php');
+
+//if ($_SERVER(request_method) == post) {
+    $u_name = $_POST[Firstname];
+    $u_password = md5($_POST[Password]);
+    $selectquery = "select * from Phptask where name='$u_name' and password='$u_password'";
+    $result = mysqli_query($dbcon, $selectquery);
+    echo mysqli_num_rows($result);
+    if (mysqli_num_rows($result) == 1) {
+        session_start();
+        echo mysqli_num_rows($result);
+        $_SESSION['username'] = $u_name;
+        header('location:account.php');
+    } else {
+        echo "<h2>'wrong user'</h2>";
+    }
+
 ?>
