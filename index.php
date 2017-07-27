7cdbbfc727adf311d9959a476c8331a8a6e5972c
OX SoftwareS <ramanathan@oxsoftwares.com>
2017-07-27 10:43:09 +0530

    display user data on the home page

---
+++ index.php
@@ -0,0 +1,30 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Welcome!</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="formstyle.css">
+    </head>
+    <body bgcolor="#F8F8FF">
+        <h1>Welcome</h1> 
+        <a href="register.php"><input type="submit" name="submit" id="register" value="Register" /></a>
+        <a href="login.php"><input type="submit" name="submit" id="login" value="Login" /></a>
+        <?php
+        include 'dbconnection.php';
+        $userinfo="select name,mobile,email from Phptask";
+        $result= mysqli_query($dbcon, $userinfo);
+        echo "<table width='50%' align='center' border='2px' ><th>Name</th><th>Mobile</th><th>Email</th>";
+        while($rows= mysqli_fetch_array($result))
+        {
+                echo '<tr align="center">';
+                echo '<td >' . $rows['name'] . '</td>';
+                echo '<td>' . $rows['mobile'] . '</td>';
+                echo '<td>' . $rows['email'] . '</td>';
+               
+//                echo '<td>' . $rows['profile'] . '</td>';
+                echo '</tr>';
+        }
+        ?>
+        
+    </body>
+</html>
