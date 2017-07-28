8bb102df842a1d436b68a0f01c15078cf2aca9a1
OX SoftwareS <ramanathan@oxsoftwares.com>
2017-07-28 20:05:30 +0530

    Add echo mysqli_num_rows(); line for check what row you are selected in login

--- logincheck.php
+++ logincheck.php
@@ -3,18 +3,17 @@
 include('dbconnection.php');
 
 //if ($_SERVER(request_method) == post) {
-    $u_name = $_POST[Firstname];
-    $u_password = md5($_POST[Password]);
-    $selectquery = "select * from Phptask where name='$u_name' and password='$u_password'";
-    $result = mysqli_query($dbcon, $selectquery);
+$u_name = $_POST[Firstname];
+$u_password = md5($_POST[Password]);
+$selectquery = "select * from Phptask where name='$u_name' and password='$u_password'";
+$result = mysqli_query($dbcon, $selectquery);
+echo mysqli_num_rows($result);
+if (mysqli_num_rows($result) == 1) {
+    session_start();
     echo mysqli_num_rows($result);
-    if (mysqli_num_rows($result) == 1) {
-        session_start();
-        echo mysqli_num_rows($result);
-        $_SESSION['username'] = $u_name;
-        header('location:account.php');
-    } else {
-        echo "<h2>'wrong user'</h2>";
-    }
-
+    $_SESSION['username'] = $u_name;
+    header('location:account.php');
+} else {
+    echo "<h2>'wrong user'</h2>";
+}
 ?>
